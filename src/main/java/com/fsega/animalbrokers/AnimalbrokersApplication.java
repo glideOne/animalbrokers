b1e763f024973d71f9bea4156ab6044a87e2b812
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2021-04-24 19:23:12 +0300

    First commit

---
+++ src/main/java/com/fsega/animalbrokers/AnimalbrokersApplication.java
@@ -0,0 +1,13 @@
+package com.fsega.animalbrokers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalbrokersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalbrokersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fsega/animalbrokers/AnimalbrokersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsega.animalbrokers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalbrokersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
