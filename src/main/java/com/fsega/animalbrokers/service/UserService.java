3f25285bce639e5afcd072f1e3887fb7a6e64810
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2021-05-04 14:32:50 +0300

    Changed port to 8090.

--- src/main/java/com/fsega/animalbrokers/service/UserService.java
+++ src/main/java/com/fsega/animalbrokers/service/UserService.java
@@ -8,8 +8,6 @@ import com.fsega.animalbrokers.utils.exception.BadRequestException;
 import com.fsega.animalbrokers.utils.exception.ExceptionType;
 import com.fsega.animalbrokers.utils.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
