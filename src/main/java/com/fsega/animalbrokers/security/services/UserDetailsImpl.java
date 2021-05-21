00bcc275ac6ee0081c8b2e20f09130df1ac73646
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2021-05-21 23:10:38 +0300

    ÈFixed getAuthorities.

--- src/main/java/com/fsega/animalbrokers/security/services/UserDetailsImpl.java
+++ src/main/java/com/fsega/animalbrokers/security/services/UserDetailsImpl.java
@@ -55,7 +55,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     public UUID getId() {
